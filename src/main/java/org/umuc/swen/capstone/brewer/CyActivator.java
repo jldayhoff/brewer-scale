f759897c65711fe44b0ae68fccd538b5338b6d5d
Chris Wancowicz <cwancowicz@gmail.com>
2016-09-22 07:11:19 -0400

    Initial check in of code for SWEN 670 Brewer Scale project

---
+++ src/main/java/org/umuc/swen/capstone/brewer/CyActivator.java
@@ -0,0 +1,27 @@
+package org.umuc.swen.capstone.brewer;
+
+import java.util.Properties;
+import org.cytoscape.application.swing.CyAction;
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.application.swing.CytoPanelComponent;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class CyActivator extends AbstractCyActivator {
+	public CyActivator() {
+		super();
+	}
+
+
+	public void start(BundleContext bc) {
+		CySwingApplication cytoscapeDesktopService = getService(bc,CySwingApplication.class);
+		
+		MyControlPanel myControlPanel = new MyControlPanel();
+		ControlPanelAction controlPanelAction = new ControlPanelAction(cytoscapeDesktopService,myControlPanel);
+
+		registerService(bc,myControlPanel,CytoPanelComponent.class, new Properties());
+		registerService(bc,controlPanelAction,CyAction.class, new Properties());
+	}
+}
+
