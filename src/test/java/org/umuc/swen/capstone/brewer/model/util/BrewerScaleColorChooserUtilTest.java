b0ae70a42f2cbdc4f1886fc6c71ac574da80e803
Chris Wancowicz <cwancowicz@gmail.com>
2016-09-29 19:46:54 -0400

    Created enum type for ScaleType

--- src/test/java/org/umuc/swen/capstone/brewer/model/util/BrewerScaleColorChooserUtilTest.java
+++ src/test/java/org/umuc/swen/capstone/brewer/model/util/BrewerScaleColorChooserUtilTest.java
@@ -2,6 +2,7 @@ package org.umuc.swen.capstone.brewer.model.util;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
@@ -14,6 +15,7 @@ import org.umuc.swen.capstone.brewer.model.domain.brewercolor.BrewerColorScale;
 public class BrewerScaleColorChooserUtilTest {
 
   private BrewerScaleColorChooserUtil brewerScaleColorChooserUtil;
+  private final static Random RANDOM = new Random();
 
   @Before
   public void setUp() {
@@ -22,11 +24,14 @@ public class BrewerScaleColorChooserUtilTest {
 
   @Test
   public void shouldCreateListOfBrewerColorScales() {
+    int index = RANDOM.nextInt(33);
     List<BrewerColorScale> brewerColorScale = brewerScaleColorChooserUtil.getBrewerColorScale();
     assertNotNull(brewerColorScale);
     assertTrue(brewerColorScale.size() >= 1);
-    assertNotNull(brewerColorScale.get(0).getScales());
-    assertTrue(brewerColorScale.get(0).getScales().size() >= 1);
+    assertNotNull("ScaleType is null", brewerColorScale.get(index).getType());
+    assertNotNull("Name is null", brewerColorScale.get(index).getName());
+    assertNotNull("Scales is null", brewerColorScale.get(index).getScales());
+    assertTrue(brewerColorScale.get(index).getScales().size() >= 1);
   }
 
   @Test
