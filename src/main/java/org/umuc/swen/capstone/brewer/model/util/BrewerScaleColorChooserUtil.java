278e5382bfb3a35f8294b5f3e5b1753a7eb974d7
Chris Wancowicz <cwancowicz@gmail.com>
2016-09-28 21:53:41 -0400

    * converted the rest of the brewer scale colors to JSON. 35 total styles each with 5-10 scales * added Map for better manipulation of colors

--- src/main/java/org/umuc/swen/capstone/brewer/model/util/BrewerScaleColorChooserUtil.java
+++ src/main/java/org/umuc/swen/capstone/brewer/model/util/BrewerScaleColorChooserUtil.java
@@ -1,11 +1,17 @@
 package org.umuc.swen.capstone.brewer.model.util;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.umuc.swen.capstone.brewer.model.domain.brewercolor.BrewerColorScale;
 
 /**
@@ -14,28 +20,43 @@ import org.umuc.swen.capstone.brewer.model.domain.brewercolor.BrewerColorScale;
 public class BrewerScaleColorChooserUtil {
 
   private static final String colorBrewerJsonFilePath = "colorbrewer.json";
-  private static BrewerColorScale brewerColorScale;
+  private static List<BrewerColorScale> brewerColorScales;
+  private static Map<String, BrewerColorScale> brewerColorScaleMap;
 
   /**
    * Reads file only once and returns the {@link BrewerColorScale} from the first time the json file was read
    *
-   * @return {@link BrewerColorScale}
+   * @return {@link List} of {@link BrewerColorScale}
    */
-  public BrewerColorScale getBrewerColorScale() {
+  public List<BrewerColorScale> getBrewerColorScale() {
     return getColorBrewerScaleFromFile();
   }
 
-  private BrewerColorScale getColorBrewerScaleFromFile() {
+  /**
+   * Converts the {@link List} of {@link BrewerColorScale} to a {@link Map} for easier manipulation
+   *
+   * @return {@link Map<String, BrewerColorScale>}
+   */
+  public Map<String, BrewerColorScale> getBrewerColorScaleMap() {
+    if (Objects.isNull(brewerColorScaleMap)) {
+      brewerColorScaleMap = getBrewerColorScale()
+              .stream()
+              .collect(Collectors.toMap(BrewerColorScale::getName, Function.identity()));
+    }
+    return brewerColorScaleMap;
+  }
 
-    if (Objects.isNull(brewerColorScale)) {
+  private List<BrewerColorScale> getColorBrewerScaleFromFile() {
+
+    if (Objects.isNull(brewerColorScales)) {
       try {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(colorBrewerJsonFilePath);
         Reader reader = new InputStreamReader(inputStream, "UTF-8");
-        brewerColorScale = new Gson().fromJson(reader, BrewerColorScale.class);
+        brewerColorScales = new Gson().fromJson(reader, new TypeToken<ArrayList<BrewerColorScale>>(){}.getType());
       } catch (UnsupportedEncodingException e) {
         e.printStackTrace();
       }
     }
-    return brewerColorScale;
+    return brewerColorScales;
   }
 }
